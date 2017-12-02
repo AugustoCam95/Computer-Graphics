39e9ba5178cca92b3035f5eb0b25c5832fd23ace
IronBaron <augustocam95@gmail.com>
2017-12-02 16:47:07 -0300

    Completando parte 5

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from objectcg import ObjectCG
 
 class Camera:
 
@@ -24,31 +24,46 @@ class Camera:
 
     def find_world_camera(self, eye, lookat, avup):
         kc, jc, ic = self.find_kij(eye, lookat, avup)
-        iwc = np.append(ic, -np.dot(ic, eye[:3]))
-        jwc = np.append(jc, -np.dot(jc, eye[:3]))
-        kwc = np.append(kc, -np.dot(kc, eye[:3]))
+
+        iwc = np.append(ic, ( -1 * ( np.sqrt( np.dot(ic, eye) ) ) ) )
+        jwc = np.append(jc, ( -1 * ( np.sqrt( np.dot(jc, eye) ) ) ) )
+        kwc = np.append(kc, ( -1 * ( np.sqrt( np.dot(kc, eye) ) ) ) )
+
         rowIdentity = np.array([0, 0, 0, 1])
         world_camera = np.vstack([iwc, jwc, kwc, rowIdentity])
         return world_camera
 
-    def apply_m_camera(self, mWC, objectsList):
+    def apply_m_camera(self, mWC, objectsListWorld):
+
+        objectsList = np.array([])
+
         # Percorre os objetos do cenario
-        for obj in objectsList:
+        for obj in objectsListWorld:
+            objTemp = ObjectCG([])
+
             vertices = obj.get_vertices()
-            mTemp = np.ones((4, 1))
+
+            verticesTemp = np.empty((0, 3), float)
 
             # Para cada vertice do objeto converte para coordenadas de camera
             for v in vertices:
+                mTemp = np.ones((4, 1))
+
                 mTemp[0, 0] = v[0]
                 mTemp[1, 0] = v[1]
                 mTemp[2, 0] = v[2]
 
                 mTemp = np.dot(mWC, mTemp)
 
-                v[0] = mTemp[0, 0]
-                v[1] = mTemp[1, 0]
-                v[2] = mTemp[2, 0]
+                x = mTemp[0, 0]
+                y = mTemp[1, 0]
+                z = mTemp[2, 0]
 
-            obj.set_vertices(vertices)
+                verticesTemp = np.append(verticesTemp, np.array([[x, y, z]]), axis=0)
+
+
+            objTemp.set_vertices(verticesTemp)
+
+        objectsList = np.append(objectsList, objTemp)
 
         return objectsList
