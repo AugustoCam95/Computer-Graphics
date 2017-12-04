33ef6465078ea9e1e634b6b6544b7b656bcb5616
José Augusto <augustocam95@gmail.com>
2017-12-04 14:42:04 -0300

    Update ap2.py

--- ap2/ap2.py
+++ ap2/ap2.py
@@ -1,7 +1,11 @@
+import np as np
 import numpy as np
 import math as mh
 
-matricula = '370019'
+matricula = '388112'
+#matricula = '370019'
+#matricula = '391941'
+#matricula = '353003'
 
 F,E,D,C,B,A = matricula
 
@@ -30,8 +34,8 @@ print("\n")
 
 LAt = P4-np.array([0,0,((L*(mh.sqrt(6)))/6)])
 Eye = np.array([A-5,B+L,(L*(mh.sqrt(6)))/6])
-avup = (LAt)+(0,0,10.2)
-'avup = P4'
+'avup = (LAt)+(0,0,10.2)'
+avup = P4
 
 print("Avup:{}".format(avup))
 print("LAt:{}".format(LAt))
@@ -46,6 +50,8 @@ print("VUP:")
 print(vup)
 print("\n")
 i = np.cross(vup, kc)
+print("Izao")
+print(i)
 ic = (i / np.linalg.norm(i))
 jc = np.cross(kc, ic)
 
@@ -68,9 +74,9 @@ print(camera_world)
 
 print("\n")
 
-iwc = np.append(ic, -np.dot(ic, Eye[:3]))
-jwc = np.append(jc, -np.dot(jc, Eye[:3]))
-kwc = np.append(kc, -np.dot(kc, Eye[:3]))
+iwc = np.append(ic, -1*(np.dot(ic, Eye[:3])))
+jwc = np.append(jc, -1*(np.dot(jc, Eye[:3])))
+kwc = np.append(kc, -1*(np.dot(kc, Eye[:3])))
 rowIdentity = np.array([0, 0, 0, 1])
 world_camera = np.vstack([iwc, jwc, kwc, rowIdentity])
 
@@ -83,20 +89,85 @@ P2l = np.append(P2,1)
 P3l = np.append(P3,1)
 P4l = np.append(P4,1)
 
-P1w = world_camera.dot(P1l)[:4]
-P2w = world_camera.dot(P2l)[:4]
-P3w = world_camera.dot(P3l)[:4]
-P4w = world_camera.dot(P4l)[:4]
+P1c = world_camera.dot(P1l)[:3]
+P2c = world_camera.dot(P2l)[:3]
+P3c = world_camera.dot(P3l)[:3]
+P4c = world_camera.dot(P4l)[:3 ]
 
 print("\n")
-print("P1:")
-print(P1w)
+print("P1c:")
+print(P1c)
 print("\n")
-print("P2:")
-print(P2w)
+print("P2c:")
+print(P2c)
 print("\n")
-print("P3:")
-print(P3w)
+print("P3c:")
+print(P3c)
 print("\n")
-print("P4:")
-print(P4w)
+print("P4c:")
+print(P4c)
+print("\n")
+
+w11 = P4c-P1c
+w12 = P3c-P1c
+Nzao = np.cross(w11,w12)
+n = (Nzao/np.linalg.norm(Nzao))
+print("Nzao")
+print(Nzao)
+print("\n")
+print("n normalizado:")
+print(n)
+print("\n")
+
+pw =np.array([23.5,12.64,4.7,1])
+fw =np.array([12,24,46,1])
+#pw= np.array([29,11.77,4.2817,1])
+#fw= np.array([A+10,B+L,2*L,1])
+#pw= np.array([24.5,16.79,-69.97,1])
+#fw= np.array([11,31,54,1])
+pc = world_camera.dot(pw)[:3]
+fc = world_camera.dot(fw)[:3]
+print("pc:")
+print(pc)
+print("fc:")
+print(fc)
+print("\n")
+
+t = (np.dot(P1c,n)/np.dot(pc,n))
+print("t:")
+print(t)
+print("\n")
+
+pint = t*pc
+print("pint:")
+print(pint)
+print("\n")
+
+lzao =  fc - pint
+print("lzao")
+print(lzao)
+print("\n")
+l = (lzao/np.linalg.norm(lzao))
+print("l:")
+print(l)
+print("\n")
+
+vzao = -pint
+print("vzao:")
+print(vzao)
+print("\n")
+v = (vzao/np.linalg.norm(vzao))
+print("v:")
+print(v)
+print("\n")
+
+prod_esc = l[0]*n[0] + l[1]*n[1] + l[2]*n[2]
+print (n)
+print (prod_esc)
+r = []
+r.append(2*n[0] * prod_esc - l[0])
+r.append(2*n[1] * prod_esc - l[1])
+r.append(2*n[2] * prod_esc - l[2])
+#r = (np.dot(l,n)*(2*n))-l
+print("r")
+print(r)
